d527ca0c7c38f6259bf2298f36fc3d057b68c601
Collins omondi <collinsomondi825@gmail.com>
2021-06-04 21:53:54 +0300

    Add Images

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [
+    './*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    },
+    colors: {
+      "deep-blue" : "#0a192f",
+      "light-gray" : "#ccd6f6",
+      "deep-gray" : "#8892b0",
+      "green" : "#64ffda",
+      "bg-blue-base" : "#172a45",
+      "bg-dark-blue" : "#020c1b",
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
